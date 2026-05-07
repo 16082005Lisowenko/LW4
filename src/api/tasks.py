d42dcc56ade6dc7f3c66ecac214dc9b90d9087f6
Backart <artem.maksymenko2000@gmail.com>
2026-05-07 16:39:12 +0300

    feat(api): add tasks endpoints

---
+++ src/api/tasks.py
@@ -0,0 +1,4 @@
+def get_tasks():
+    return "GET /tasks"
+def post_tasks():
+    return "POST /tasks"
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 from .user import User
 from .task import Task, TaskStatus
 from .task_service import TaskService
+from .tasks import get_tasks, post_tasks
