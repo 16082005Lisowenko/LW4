9a69498c9a90f44c4d038943dea477d4df68c36b
Backart <artem.maksymenko2000@gmail.com>
2026-05-07 16:38:16 +0300

    feat(services): add TaskService

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,3 @@
 from .user import User
 from .task import Task, TaskStatus
+from .task_service import TaskService
---
+++ src/services/task_service.py
@@ -0,0 +1,7 @@
+class TaskService:
+    def create_task(self):
+        pass
+    def get_task_by_id(self):
+        pass
+    def assign_task(self):
+        pass
